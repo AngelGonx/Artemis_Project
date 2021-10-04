c66635567b3b0aa915dcc1273a6a924d2b0c4331
Ángel González <42718651+AngelGonx@users.noreply.github.com>
2021-10-03 19:31:02 -0500

    Project files.

---
+++ Artemis_Project/src/artemis_project/Artemis_Project.java
@@ -0,0 +1,12 @@
+package artemis_project;
+/**
+ *
+ * @author Artemis Team
+ * 
+ */
+public class Artemis_Project {
+    public static void main(String[] args) {
+        window w = new window();
+        w.setVisible(true);
+    } 
+}
